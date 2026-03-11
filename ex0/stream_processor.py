47e395643a886777c4a46a8a432de1c28da47d2a
cedrichenrotel <cedric.henrotel@gmail.com>
2026-03-11 18:44:15 +0100

    deuxieme partie ex0 finis

--- ex0/stream_processor.py
+++ ex0/stream_processor.py
@@ -5,15 +5,15 @@
 #                                                      :::      ::::::::    #
 #  stream_processor.py                               :+:      :+:    :+:    #
 #                                                  +:+ +:+         +:+      #
-#  By: cehenrot <cehenrot@student.42.fr>         +#+  +:+       +#+         #
+#  By: cehenrot <cehenrot@student.42lyon.fr>     +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 2026/03/11 08:29:09 by cehenrot        #+#    #+#               #
-#  Updated: 2026/03/11 13:52:46 by cehenrot        ###   ########.fr        #
+#  Updated: 2026/03/11 18:43:04 by cehenrot        ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
 
 from abc import ABC, abstractmethod
-from typing import Any, List, Dict, Union, Optional
+from typing import Any
 
 
 class DataProcessor(ABC):
@@ -37,17 +37,44 @@ class NumericProcessor(DataProcessor):
         sumt = sum(data)
         avg = sumt / len(data)
         rst = (f"Processed {len(data)} numeric values, sum={sumt}"
-               f", avg={avg}")
+               f", avg={avg:.2f}")
         return self.format_output(rst)
 
     def validate(self, data: Any) -> bool:
+
         for i in range(len(data)):
             try:
-                int(data[i])
+                isinstance(data[i], (int, float))
             except ValueError as e:
-                print(f"{e}\nn is not an (int): {data[i]} [KO]")
+                print(f"{e} [KO]")
                 return False
-        print("Validation: Numeric data verified")
+        return True
+
+    def format_output(self, result: str) -> str:
+        return f"Output: {result}"
+
+
+class TextProcessor(DataProcessor):
+
+    def process(self, data: Any) -> str:
+
+        try:
+            list_word = data.split()
+        except AttributeError as e:
+            print(e)
+
+        nb_c = len(data)
+        nb_word = len(list_word)
+        rst = (f"Processed text: {nb_c} characters, {nb_word} words")
+        return self.format_output(rst)
+
+    def validate(self, data: any) -> bool:
+
+        try:
+            _ = data.split()
+        except (AttributeError) as e:
+            print(f"{e} [KO]")
+            return False
         return True
 
     def format_output(self, result: str) -> str:
@@ -55,8 +82,28 @@ class NumericProcessor(DataProcessor):
 
 
 def main():
-    rst = NumericProcessor().process([1, 2, 3, 4, 5])
-    print(rst)
+
+    print("=== CODE NEXUS - DATA PROCESSOR FOUNDATION ===")
+
+    print("\nInitializing Numeric Processor...")
+
+    num = [1, 2, 3, 4, 5]
+    proc = NumericProcessor()
+
+    if proc.validate(num):
+        print(f"Processing data: {num}")
+        print("Validation: Numeric data verified")
+        print(proc.process(num))
+
+    print("\nInitializing Text Processor...")
+
+    s = 42
+    proc = TextProcessor()
+
+    if proc.validate(s):
+        print(f"Processing data: {s}")
+        print("Validation: Text data verified")
+        print(proc.process(s))
 
 
 if __name__ == "__main__":
