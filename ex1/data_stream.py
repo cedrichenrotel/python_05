aefd3702c015e022633f4d01c89bde40aef3d859
cedrichenrotel <cedric.henrotel@gmail.com>
2026-03-12 18:39:39 +0100

    premier ex finis

--- ex1/data_stream.py
+++ ex1/data_stream.py
@@ -5,10 +5,10 @@
 #                                                      :::      ::::::::    #
 #  data_stream.py                                    :+:      :+:    :+:    #
 #                                                  +:+ +:+         +:+      #
-#  By: cehenrot <cehenrot@student.42.fr>         +#+  +:+       +#+         #
+#  By: cehenrot <cehenrot@student.42lyon.fr>     +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 2026/03/12 11:26:07 by cehenrot        #+#    #+#               #
-#  Updated: 2026/03/12 13:29:23 by cehenrot        ###   ########.fr        #
+#  Updated: 2026/03/12 18:38:28 by cehenrot        ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
 
@@ -22,31 +22,54 @@ class DataStream(ABC):
         self.stream_id = stream_id
 
     @abstractmethod
-    def process_batch(self, data_batch: List[Any]) -> str:                  #traite liste
-        pass
-
-    def filter_data(self, data_batch: List[Any], criteria: Optional[str]    #filtre data
-                    = None) -> List[Any]:
-        pass
-
-    def get_stats(self) -> Dict[str, Union[str, int, float]]:               #retourne les statistiques
-        pass
-
-
-class SensorStream(DataStream): #capteur de temperature
-
-    def __init__(self, stream_id: str) -> None:
-
-
     def process_batch(self, data_batch: List[Any]) -> str:
-
+        pass
 
     def filter_data(self, data_batch: List[Any], criteria: Optional[str]
                     = None) -> List[Any]:
-        
+        pass
 
     def get_stats(self) -> Dict[str, Union[str, int, float]]:
+        pass
 
+
+class SensorStream(DataStream):
+
+    def __init__(self, stream_id: str) -> None:
+        super().__init__(stream_id)
+        self.type = "Environmental Data"
+        self.temp = 0.0
+        self.len_tab = 0
+
+        print(f"Stream ID: {stream_id}, Type: {self.type}")
+
+    def process_batch(self, data_batch: List[Any]) -> str:
+
+        self.len_tab = len(data_batch)
+
+        for i in range(self.len_tab):
+            data = data_batch[i].split(':')
+            if data[0] == 'temp':
+                self.temp = float(data[1])
+
+        return (f"Sensor analysis: {len(data_batch)} readings "
+                f"processed, avg temp: {self.temp}°C")
+
+    def filter_data(self, data_batch: List[Any], criteria: Optional[str]
+                    = None) -> List[Any]:
+        lst_err = []
+        for i in range(self.len_tab):
+            data = data_batch[i].split(':')
+            if data[0] == 'temp':
+                self.temp = float(data[1])
+
+        if self.temp >= 100:
+            lst_err.append(self.temp)
+
+        return lst_err
+
+    def get_stats(self) -> Dict[str, Union[str, int, float]]:
+        return {"type": self.type, "temp": self.temp, "len_tab": self.len_tab}
 
 
 def main() -> None:
@@ -55,7 +78,11 @@ def main() -> None:
     print("\nInitializing Sensor Stream...")
 
     stream_id = "SENSOR_001"
-    capteur =["temp:22.5", "humidity:65", "pressure:1013"]
+    capteur = ["temp:22.5", "humidity:65", "pressure:1013"]
     proc = SensorStream(stream_id)
-
+    print(f"Processing sensor batch: [{', '.join(capteur)}]")
     print(proc.process_batch(capteur))
+
+
+if __name__ == "__main__":
+    main()
